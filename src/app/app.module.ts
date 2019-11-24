aba2f2130d929ffc1ccd5bbe883e8c02cb4e517e
Sanjeev <itSan@DESKTOP-GE9MV89>
2019-11-24 21:24:44 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './Product/product-list.component';
+import { ConvertToSpacesPipe } from './Common/convert-to-space.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
